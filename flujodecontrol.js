0f9e1a92a0c626b4afd8d880db1cfbac87c0854a
Angeles Ri0z <rios.zendejas@gmail.com>
2022-01-16 00:54:49 -0600

    final

--- flujodecontrol.js
+++ flujodecontrol.js
@@ -21,7 +21,7 @@ function businessHours(dayNumber,hourNumber) {
   }
   //getDayNumber();
   
-  //alert("De acuerdo a los datos ingresados el dia cayo en : " + getDayNumber(parseInt(prompt("En que dia inició el año siendo 0(lunes) y 6(domingo))")),parseInt(prompt("Elige un numero del 0 al 365"))));
+  //window.alert("De acuerdo a los datos ingresados el dia cayo en : " + getDayNumber(parseInt(prompt("En que dia inició el año siendo 0(lunes) y 6(domingo))")),parseInt(prompt("Elige un numero del 0 al 365"))));
   
   //PARTE 3
   
